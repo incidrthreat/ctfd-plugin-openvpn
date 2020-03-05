4c1e4de1bbf612ec4a27dea1b163cf360ae18920
incidrthreat <incidrthreat@gmail.com>
2020-03-05 08:48:52 -0600

    working PoC

--- __init__.py
+++ __init__.py
@@ -2,12 +2,15 @@ from flask import render_template, Blueprint
 from CTFd.utils.decorators import admins_only
 from CTFd.plugins import register_plugin_assets_directory
 
+assets = Blueprint("assets", __name__, template_folder="assets")
+
 def load(app):
     print("OpenVPN-Cfg plugin is ready!")
-    assets = Blueprint("assets", __name__, template_folder="assets")
     app.register_blueprint(assets)
     register_plugin_assets_directory(app, base_path='/plugins/ctfd-plugin-openvpn/assets/')
-    @assets.route('/admin/openvpn', methods=['GET'])
-    #@admins_only
-    def ovpncfg():
-        return render_template('config.html') 
+
+
+@assets.route('/admin/openvpn', methods=['GET'])
+#@admins_only
+def ovpncfg():
+    return render_template('config.html') 
