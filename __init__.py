b11aa156f650c3f27cd98653ec794a1173626ec9
incidrthreat <incidrthreat@gmail.com>
2020-03-05 08:34:56 -0600

    small edits

--- __init__.py
+++ __init__.py
@@ -1,10 +1,12 @@
-#from flask import render_template, Blueprint
-#from CTFd.models import db, Users
-#from sqlalchemy.sql import or_
-#from jinja2 import Template
-#import sys
-
-# The load method is the entry point of our plugin, as described in the docs
+from flask import render_template, Blueprint
+from CTFd.utils.decorators import admins_only
+from CTFd.plugins import register_plugin_assets_directory
 
 def load(app):
-    print("OpenVPN-Cfg plugin is ready!")
+    assets = Blueprint("assets", __name__, template_folder="assets")
+    app.register_blueprint(assets)
+    register_plugin_assets_directory(app, base_path='/plugins/ctfd-plugin-openvpn/assets/')
+    @assets.route('/admin/openvpn', methods=['GET'])
+    #@admins_only
+    def ovpncfg():
+        return render_template('config.html')
